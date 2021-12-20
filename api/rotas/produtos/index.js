12df44608e96bb934c98cfbd222d97e148145e3d
diegoCasa <diegochueri@gmail.com>
2021-12-19 20:30:07 -0400

    Adicionando rotas para gerenciamento de produtos

--- api/rotas/produtos/index.js
+++ api/rotas/produtos/index.js
@@ -1,11 +1,38 @@
 const roteador = require('express').Router({mergeParams: true})
+const Produto = require('./Produto')
 const Tabela = require('./TabelaProduto')
 
 roteador.get('/', async (req, res) => {
-    const produtos = await Tabela.listar(req.params.idFornecedor)
+    const produtos = await Tabela.listar(req.fornecedor.id)
     res.send(
         JSON.stringify(produtos)
     )
+})
+
+roteador.post('/', async (req, res, proximo) => {
+    try {
+        const idFornecedor = req.fornecedor.id
+        const corpo = req.body
+        const dados = Object.assign({}, corpo, {fornecedor: idFornecedor})
+        const produto = new Produto(dados)
+        await produto.criar()
+        res.status(201)
+        res.send(produto)
+    } catch (erro) {
+        proximo(erro)
+    }
+})
+
+roteador.delete('/:id', async (req, res) => {
+    const dados = {
+        id: req.params.id,
+        fornecedor: req.fornecedor.id
+    }
+
+    const produto = new Produto(dados)
+    await produto.apagar()
+    res.status(204)
+    res.end()
 })
 
 module.exports = roteador
